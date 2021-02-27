331e0a0b0bc5e767913d39a47b8578367ee3b3c1
asnakech1991 <asnakech.anteneh21@gmail.com>
2021-02-27 13:35:55 -0800

    Initial commit

---
+++ build/precache-manifest.dbcc7e436afc9ea945930e4a7feb1865.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "705ce9095125d46a6f32aaff31b97d70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2acaf3cc36be69b1ee4",
+    "url": "/static/css/main.b215b06a.chunk.css"
+  },
+  {
+    "revision": "92f2502db03248df14ab",
+    "url": "/static/js/2.7199c33c.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/static/js/2.7199c33c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2acaf3cc36be69b1ee4",
+    "url": "/static/js/main.49e13fc8.chunk.js"
+  },
+  {
+    "revision": "07b33697149324db54e5",
+    "url": "/static/js/runtime-main.b8a87127.js"
+  }
+]);
